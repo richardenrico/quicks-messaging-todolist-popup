5a7112497afa3bcd719b25d30f9e3952375a8557
richardenrico <richardenrico02@gmail.com>
2024-01-04 22:27:53 +0800

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: require('./src/shared/utils/colors').COLORS 
+    },
   },
   plugins: [],
 }
