1d35af5bbd85baa67597189bd6d40faec5a81209
richardenrico <richardenrico02@gmail.com>
2024-01-04 22:28:08 +0800

    feat: add FAB

---
+++ src/shared/components/FloatingActionButton.tsx
@@ -0,0 +1,117 @@
+import {
+    ActionIcon,
+    Affix,
+    Flex,
+    Image,
+} from '@mantine/core'
+import FeatherIcon from 'assets/icon/feather_icon.svg'
+import QuestionIcon from 'assets/icon/question_icon.svg'
+import QuestionWhiteIcon from 'assets/icon/question_icon_white.svg'
+import ReaderIcon from 'assets/icon/reader_icon.svg'
+import ReaderWhiteIcon from 'assets/icon/reader_icon_white.svg'
+
+interface IFloatingActionButtonProps {
+    mode: string
+    setMode: (mode: 'task' | 'inbox' | '') => void
+    opened: boolean
+    setOpened: (opened: boolean) => void
+}
+
+function FloatingActionButton({mode, opened, setMode, setOpened}: IFloatingActionButtonProps) {
+    return (
+        <Affix position={{ bottom: 20, right: 20 }}>
+            <Flex
+                justify="flex-start"
+                align="center"
+                direction="row-reverse"
+                wrap="wrap"
+                gap={'md'}
+            >
+                {mode === '' && (
+                    <ActionIcon
+                        size={60}
+                        radius="xl"
+                        color="primary_blue"
+                        onClick={() => setOpened(!opened)}
+                    >
+                        <Image
+                            src={FeatherIcon}
+                            style={{ width: '60%', height: '60%' }}
+                        />
+                    </ActionIcon>
+                )}
+
+                {mode === 'inbox' && (
+                    <ActionIcon
+                        className="shadow-[-10px_0px_0px_0px] shadow-primary_gray_1"
+                        size={60}
+                        radius="xl"
+                        color="indicator_blue"
+                        onClick={() => setMode('')}
+                    >
+                        <Image
+                            src={QuestionWhiteIcon}
+                            style={{ width: '60%', height: '60%' }}
+                        />
+                    </ActionIcon>
+                )}
+
+                {mode === 'task' && (
+                    <ActionIcon
+                        className="shadow-[-10px_0px_0px_0px] shadow-primary_gray_1"
+                        size={60}
+                        radius="xl"
+                        color="indicator_orange"
+                        onClick={() => setMode('')}
+                    >
+                        <Image
+                            src={ReaderWhiteIcon}
+                            style={{ width: '60%', height: '60%' }}
+                        />
+                    </ActionIcon>
+                )}
+
+                {opened && (
+                    <>
+                        {mode !== 'inbox' && (
+                            <ActionIcon
+                                mr={(mode !== '') ? 'xs' : 0}
+                                size={56}
+                                radius="xl"
+                                color="#F2F2F2"
+                                onClick={() => setMode('inbox')}
+                            >
+                                <Image
+                                    src={QuestionIcon}
+                                    style={{
+                                        width: '50%',
+                                        height: '50%',
+                                    }}
+                                />
+                            </ActionIcon>
+                        )}
+                        {mode !== 'task' && (
+                            <ActionIcon
+                                mr={(mode !== '') ? 'xs' : 0}
+                                size={56}
+                                radius="xl"
+                                color="#F2F2F2"
+                                onClick={() => setMode('task')}
+                            >
+                                <Image
+                                    src={ReaderIcon}
+                                    style={{
+                                        width: '50%',
+                                        height: '50%',
+                                    }}
+                                />
+                            </ActionIcon>
+                        )}
+                    </>
+                )}
+            </Flex>
+        </Affix>
+    )
+}
+
+export default FloatingActionButton
