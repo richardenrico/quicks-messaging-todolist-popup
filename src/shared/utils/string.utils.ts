13df412aec43c4079fc5ef533a4d776851bc0d3f
richardenrico <richardenrico02@gmail.com>
2024-01-05 19:21:47 +0800

    feat: add some date formatter

---
+++ src/shared/utils/string.utils.ts
@@ -0,0 +1,25 @@
+export function getStringDate(date: Date): string {
+    const formattedDate = date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+    })
+
+    return formattedDate
+}
+
+export function getNumberDate(date: Date): string {
+    const formattedDate = date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+    })
+
+    return formattedDate
+}
