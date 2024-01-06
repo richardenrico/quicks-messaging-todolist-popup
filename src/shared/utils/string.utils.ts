2195dcfd524147236ec3aac1032a79972f43d09e
richardenrico <richardenrico02@gmail.com>
2024-01-06 20:43:11 +0800

    feat: add new time converter

--- src/shared/utils/string.utils.ts
+++ src/shared/utils/string.utils.ts
@@ -33,3 +33,32 @@ export function getHoursMinuteTime(date: Date): string {
 
     return formattedDate
 }
+
+export function getFullDayTime(date: Date): string {
+    const daysOfWeek = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ]
+    const currentDay = new Date().getDay()
+    const inputDay = date.getDay()
+
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    })
+    const formattedDate = formatter.format(date)
+
+    if (currentDay - inputDay === 0) {
+        return `Today ${formattedDate}`
+    } 
+    // else if (currentDay - inputDay === 1) {
+    //     return `Yesterday ${formattedDate}`
+    // }
+    return `${daysOfWeek[inputDay]} ${formattedDate}`
+}
