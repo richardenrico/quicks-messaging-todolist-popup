8418da05533933acbf6e229f1b082fcd65000d5e
richardenrico <richardenrico02@gmail.com>
2024-01-06 17:15:02 +0800

    feat: add ChatPopover component

---
+++ src/shared/components/ChatBubble.tsx
@@ -0,0 +1,67 @@
+import { Stack, Flex, Paper, Text } from '@mantine/core'
+import ChatPopover from 'shared/components/ChatPopover'
+import { getHoursMinuteTime } from 'shared/utils/string.utils'
+
+interface IChatBubbleProps {
+    mode: 'receiver' | 'sender'
+    recieverMode?: 1 | 2
+    name?: string
+    chat: string
+    time: number
+}
+
+function ChatBubble({
+    chat,
+    mode,
+    recieverMode = 1,
+    name,
+    time,
+}: IChatBubbleProps) {
+    return (
+        <Stack align={mode === 'sender' ? 'flex-end' : 'flex-start'} gap={4}>
+            <Text
+                fz={14}
+                fw={'bold'}
+                c={
+                    mode === 'sender'
+                        ? 'chats_purple'
+                        : recieverMode === 1
+                        ? 'chats_yellow'
+                        : 'chats_green'
+                }
+                inline
+            >
+                {name ?? 'You'}
+            </Text>
+            <Flex
+                direction={mode === 'sender' ? 'row' : 'row-reverse'}
+                justify={'flex-end'}
+            >
+                <ChatPopover mode={mode} />
+                <Paper
+                    radius={5}
+                    p={'sm'}
+                    bg={
+                        mode === 'sender'
+                            ? 'chats_purple_dimmed'
+                            : recieverMode === 1
+                            ? 'chats_yellow_dimmed'
+                            : 'chats_green_dimmed'
+                    }
+                    className="max-w-[80%]"
+                >
+                    <Stack align="flex-start" gap={8}>
+                        <Text fz={14} c={'primary_gray_1'} inline>
+                            {chat}
+                        </Text>
+                        <Text fz={14} c={'primary_gray_1'} inline>
+                            {getHoursMinuteTime(new Date(time))}
+                        </Text>
+                    </Stack>
+                </Paper>
+            </Flex>
+        </Stack>
+    )
+}
+
+export default ChatBubble
