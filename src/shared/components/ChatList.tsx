ef2214e1b4b030f2c54b761683580e85f336478f
richardenrico <richardenrico02@gmail.com>
2024-01-08 15:56:24 +0800

    feat: integrate chat list

--- src/shared/components/ChatList.tsx
+++ src/shared/components/ChatList.tsx
@@ -1,13 +1,32 @@
-import { Stack, TextInput, ScrollArea } from '@mantine/core'
+import {
+    Stack,
+    TextInput,
+    ScrollArea,
+} from '@mantine/core'
 import Search24BlackIcon from 'assets/icon/search_black_24px.svg'
-import { getChatListData } from 'pages/Home/data/chatData'
+import { useEffect, useState } from 'react'
+import { ListChatResponse } from 'shared/api/chat/chat'
+import useGetListChatData from 'shared/api/chat/hooks/useGetListChatData.query'
 import ChatCard from 'shared/components/ChatCard'
+import Loading from 'shared/components/Loading'
 
 interface IChatListProps {
     setDataId: (id: number) => void
 }
 
 function ChatList({ setDataId }: IChatListProps) {
+    const [data, setData] = useState<ListChatResponse>([])
+
+    const chatList = useGetListChatData()
+
+    useEffect(() => {
+        if (chatList.data) {
+            const temp: ListChatResponse = chatList.data
+
+            setData(temp)
+        }
+    }, [chatList.data, chatList.isSuccess])
+
     return (
         <Stack className="h-full">
             <TextInput
@@ -23,22 +42,30 @@ function ChatList({ setDataId }: IChatListProps) {
                     },
                 }}
             />
-            <ScrollArea className="h-full" offsetScrollbars>
-                <Stack gap={22}>
-                    {getChatListData().map((data, index) => (
-                        <ChatCard
-                            chat={data.chat}
-                            isGroup={data.isGroup}
-                            time={data.time}
-                            title={data.title}
-                            groupSender={data.groupSender}
-                            newMessage={data.newMessage}
-                            lastIndex={index !== getChatListData().length}
-                            onClick={() => setDataId(data.id)}
-                        />
-                    ))}
-                </Stack>
-            </ScrollArea>
+
+            {chatList.isSuccess === true ? (
+                <ScrollArea className="h-full" offsetScrollbars>
+                    
+                    <Stack gap={22}>
+                        {data.length > 0 && data.map((d, index) => (
+                            <ChatCard
+                                key={d.id}
+                                chat={d.chat}
+                                isGroup={d.isGroup}
+                                time={d.time}
+                                title={d.title}
+                                groupSender={d.groupSender}
+                                newMessage={d.newMessage}
+                                lastIndex={index !== data.length}
+                                onClick={() => setDataId(d.id)}
+                            />
+                        ))}
+                    </Stack>
+                </ScrollArea>
+            ) : (
+                <Loading title='Loading Chats ...'/>
+            )}
+
         </Stack>
     )
 }
