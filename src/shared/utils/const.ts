f5c79f8be728842be8fdf241d1b295df5012374a
richardenrico <richardenrico02@gmail.com>
2024-01-08 14:18:40 +0800

    feat: add some const

---
+++ src/shared/utils/const.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://my-json-server.typicode.com/richardenrico/simpul-quicks-app-data/'
+// export const BASE_URL = 'https://657c549c853beeefdb993622.mockapi.io/'
+
+export const APP_NAME = 'Simpul Quicks'
