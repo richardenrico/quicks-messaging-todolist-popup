9902733e0f460712c2a41a942c6bfca570b502a1
richardenrico <richardenrico02@gmail.com>
2024-01-08 15:58:49 +0800

    fix: change base url

--- src/shared/utils/const.ts
+++ src/shared/utils/const.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://my-json-server.typicode.com/richardenrico/simpul-quicks-app-data/'
-export const BASE_URL = 'https://657c549c853beeefdb993622.mockapi.io/api/v1/'
+export const BASE_URL = 'https://my-json-server.typicode.com/richardenrico/simpul-quicks-app-data/'
+// export const BASE_URL = 'https://657c549c853beeefdb993622.mockapi.io/api/v1/'
 
 export const APP_NAME = 'Simpul Quicks'
